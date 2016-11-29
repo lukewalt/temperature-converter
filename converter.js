0b7c3505cadee181a3d332cd31671820c45bef22
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-29 16:02:34 -0600

    collected DOM elements and set them to global variables

--- converter.js
+++ converter.js
@@ -1,40 +1,46 @@
 
+// Get a references in the DOM
+var userInput = document.getElementById("user-input");
+var convButton = document.getElementById("converter");
+var convtdTemp = document.getElementById("conv-temp");
+
+
+
 var clearUserInput = function() {
-  document.getElementById('user-input').value = "";
+  userInput.value = "";
 }
 
 function toCelsius (num) {
-  num = document.getElementById('user-input').value;
-  document.getElementById('conv-temp').innerHTML = (num - 32) / 1.8;
+  num = userInput.value;
+  convtdTemp.innerHTML = (num - 32) / 1.8;
 }
 
 function toFahrenheit (num) {
-  num = document.getElementById('user-input').value;
-  document.getElementById('conv-temp').innerHTML = (num * 1.8) + 32;
+  num = userInput.value;
+  convtdTemp.innerHTML = (num * 1.8) + 32;
 }
-
-
-// Get a reference to the button element in the DOM
-var convButton = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-
-}
-
-//event handler that detects if 'enter' was pressed
-function inputKeyUp(e) {
-    e.which = e.which || e.keyCode;
-    if(e.which == 13) {
-        // submit
-    }
+  var cf_select = document.getElementById("cf_select");
+  if(cf_select.value === "celsius") {
+    toCelsius();
+  } else {
+    toFahrenheit();
+  }
 }
 
 // Assign a function to be executed when the button is clicked
-//button.addEventListener("click", determineConverter);
-
-
+var showConvertedNumber = function() {
+  convButton.addEventListener("click", determineConverter);
+}
 
 // [color the value] document.getElementById("myH2").style.color = "#ff0000";
+//event handler that detects if 'enter' was pressed
+// function inputKeyUp(e) {
+//     e.which = e.which || e.keyCode;
+//     if(e.which == 13) {
+//         // submit
+//     }
+// }
