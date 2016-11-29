2d32cdbe16d0519e82de4fb4265ae208b280894e
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-29 11:24:02 -0600

    added clear button and 'enter, event handler on input field

--- converter.js
+++ converter.js
@@ -9,6 +9,13 @@ function toFahrenheit () {
 // Get a reference to the button element in the DOM
 var convButton = document.getElementById("converter");
 
+//event handler that detects if 'enter' was hit
+function inputKeyUp(e) {
+    e.which = e.which || e.keyCode;
+    if(e.which == 13) {
+        // submit
+    }
+}
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
