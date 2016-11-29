113d36051513b1ffe2f380d3c6afd4e8f2afa854
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-29 17:02:05 -0600

    added eventListener to convert button

--- converter.js
+++ converter.js
@@ -1,9 +1,9 @@
 
 // Get a references in the DOM
 var userInput = document.getElementById("user-input");
+
 var convButton = document.getElementById("converter");
 var convtdTemp = document.getElementById("conv-temp");
-
 
 
 var clearUserInput = function() {
@@ -22,19 +22,21 @@ function toFahrenheit (num) {
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
+
 function determineConverter (clickEvent) {
-  var cf_select = document.getElementById("cf_select");
+  var cf_select = document.querySelector("input[name='cf_select']:checked").value;
+  console.log(cf_select);
   if(cf_select.value === "celsius") {
     toCelsius();
-  } else {
+  } else if (cf_select.value === "fahrenheit") {
     toFahrenheit();
   }
 }
 
+
 // Assign a function to be executed when the button is clicked
-var showConvertedNumber = function() {
-  convButton.addEventListener("click", determineConverter);
-}
+convButton.addEventListener("click", determineConverter);
+
 
 // [color the value] document.getElementById("myH2").style.color = "#ff0000";
 //event handler that detects if 'enter' was pressed
